f0a6939b56fbb903938dbc3ddfc73dd0ace4a26e
F-O-P <tayler.coon@gmail.com>
2023-12-04 16:34:38 -0600

    somewhat working carousel in HTML

--- scripts.js
+++ scripts.js
@@ -8,6 +8,12 @@ $(document).ready(function () { // When the pages loads, fetch the quotes
   console.log(window.location.pathname);
   if (window.location.pathname == "/0-homepage.html") {
     loader.classList.remove("d-none");
+    $("#videoSlider").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
     fetchQuotes();
     fetchVideos(popularVideos);
     // fetchVideos(latestVideos);
@@ -32,10 +38,10 @@ function fetchQuotes() {  // Fetch the quotes from the API
       // Loop through the quotes and add them to the page
       quotes.forEach((quote) => {
         // set the variables for the quote info
-        let pic = quote.pic_url;
-        let name = quote.name;
-        let title = quote.title;
-        let text = quote.text;
+        let quotePic = quote.pic_url;
+        let quoteName = quote.name;
+        let quoteTitle = quote.title;
+        let quoteText = quote.text;
 
         // create the carousel-item, the first one will be active
         let carouselItem = document.createElement("div");
@@ -53,26 +59,26 @@ function fetchQuotes() {  // Fetch the quotes from the API
 
         let carouselImg = document.createElement("img");
         carouselImg.classList.add("d-block", "align-self-center", "rounded-circle");
-        carouselImg.setAttribute("src", pic);
+        carouselImg.setAttribute("src", quotePic);
         carouselImg.setAttribute("alt", `Carousel Pic ${quote.id}`);
 
         let carouselCol2 = document.createElement("div");
         carouselCol2.classList.add("col-12", "col-sm-7", "offset-sm-2", "col-lg-9", "offset-lg-0");
 
-        let quoteText = document.createElement("div");
-        quoteText.classList.add("quote-text");
+        let quotedText = document.createElement("div");
+        quotedText.classList.add("quote-text");
 
         let quoteParagraph = document.createElement("p");
         quoteParagraph.classList.add("text-white");
-        quoteParagraph.innerHTML = text;
+        quoteParagraph.innerHTML = quoteText;
 
         let personName = document.createElement("h4");
         personName.classList.add("text-white", "font-weight-bold");
-        personName.innerHTML = name;
+        personName.innerHTML = quoteName;
 
         let personTitle = document.createElement("span");
         personTitle.classList.add("text-white");
-        personTitle.innerHTML = title;
+        personTitle.innerHTML = quoteTitle;
 
         // append the elements to the page and remove the loader
         quoteSection.appendChild(carouselItem);
@@ -80,10 +86,10 @@ function fetchQuotes() {  // Fetch the quotes from the API
         carouselRow.appendChild(carouselCol);
         carouselCol.appendChild(carouselImg);
         carouselRow.appendChild(carouselCol2);
-        carouselCol2.appendChild(quoteText);
-        quoteText.appendChild(quoteParagraph);
-        quoteText.appendChild(personName);
-        quoteText.appendChild(personTitle);
+        carouselCol2.appendChild(quotedText);
+        quotedText.appendChild(quoteParagraph);
+        quotedText.appendChild(personName);
+        quotedText.appendChild(personTitle);
       });
     })
     .catch((error) => {
@@ -91,41 +97,33 @@ function fetchQuotes() {  // Fetch the quotes from the API
     });
 }
 
-function fetchVideos(section) { // Fetch the videos from the API
+function fetchVideos(section) {
   // Check to see which section is being called
-  if (section == popularVideos) {
+  if (section === popularVideos) {
     fetch("https://smileschool-api.hbtn.info/popular-tutorials")
       .then((response) => response.json())
       .then((videos) => {
         console.log(videos); // For testing purposes
 
-        loader.classList.add("d-none"); // Remove the loader & text-center
-        section.classList.remove("text-center");
-
         // Loop through the videos and add them to the page
         videos.forEach((video) => {
-
           // set the variables for the video info
           let videoPreview = video.thumb_url;
           let videoTitle = video.title;
-          let subTitle = video.sub-title;
+          // Use bracket notation for sub-title due to hyphen
+          let subTitle = video['sub-title'];
           let authorPhoto = video.author_pic_url;
           let authorName = video.author;
           let ratingStars = video.star;
           let videoDuration = video.duration;
 
-          // create the carousel-items, the first one will be active
-          let carouselItem = document.createElement("div");
-          carouselItem.classList.add("carousel-item");
-
-          if (video.id == 1) {
-            carouselItem.classList.add("active");
-          }
-
-
-        })
-
-    })
+          // create carousel with slick slider
+          
+        });
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
   } else {
     console.log("not popular videos")
   }
